3be694e4214317ca5685fffabe112d9eac70ca65
SreehariS-800 <145245791+SreehariS-800@users.noreply.github.com>
2024-02-24 00:04:52 +0530

    added guess the number.py
    added 1 additional file called guess the number.py and made hardly any changes to readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Python projects
- I used these projects to learn Python. These were provided by Tech With Tim yt,,, surely i made quite a few changes to match my taste
+
+ I used these projects to learn Python. These were provided by Tech With Tim yt,,, surely i made quite a few changes to match my taste.
---
+++ guess the number.py
@@ -0,0 +1,44 @@
+import random
+
+print("ARE U READYY TO GUESS???? \nguess within 3 tries to win the challenge!")
+
+
+from_number= input("Enter the top range: ")
+
+if from_number.isdigit():
+    from_number = int(from_number)
+elif from_number <= 0:
+    print("please type a number greater than 0")
+    quit()
+else:
+    print("Please type a numerical number")
+    quit()
+        
+        
+    
+random_number = random.randint(0,from_number)
+
+guess=0
+
+while True:
+     guess +=1
+     user_guess = input("Make guess:")
+     if user_guess.isdigit():
+         user_guess=int(user_guess)
+     else:
+         print("Please type in a number!")
+         continue
+     if user_guess == random_number:
+         print("you got it correct! the number is ",random_number)
+         break
+     elif user_guess < random_number:
+         print("Your guess was above the number")
+     else:
+         print("Your guess is below the number")
+         
+if guess <= 3:
+    print("Hurray you won the challenge!")
+else:
+    print("You did it, but failed challenge!")
+        
+
